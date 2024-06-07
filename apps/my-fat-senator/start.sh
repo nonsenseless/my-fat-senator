aab7dc50e45fd4fe64d460e1fb56716a489efa80
nym <sean@nonsenseless.net>
2024-06-07 00:53:00 -0500

    Change start.sh to use nx.

--- apps/my-fat-senator/start.sh
+++ apps/my-fat-senator/start.sh
@@ -5,5 +5,5 @@
 # run, which is why this file exists in the first place.
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
-npx prisma migrate deploy
-npm run start
+npx nx run my-fat-senator:setup
+npx nx run my-fat-senator:start
