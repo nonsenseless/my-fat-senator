1c1cb0620187a5c4f6835d4bd18cedaf1a053124
Sean Thompson <sean.thompson@susco.net>
2024-07-25 15:52:23 -0400

    Update scripts documentation.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -5,4 +5,7 @@ PORTS=8080:8080
 TAG=my-fat-senator
 
 docker build -t $TAG .
+# dotenv only works in dev; docker is set to production 
+# so we need to manually set a value for the container
+# The value for local dev is irrelevant
 docker run -it -p $PORTS --env SESSION_SECRET=$SESSION_SECRET $TAG
