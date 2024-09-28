8e71522a6283d495121c45c6a546314eccaca582
Sean Thompson <sean.thompson@susco.net>
2024-09-28 17:59:24 -0500

    Set up ag grid

--- packages/my-fat-senator/app/routes/_admin.vote-types.tsx
+++ packages/my-fat-senator/app/routes/_admin.vote-types.tsx
@@ -1,6 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 import { json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
+import { ColDef } from 'ag-grid-community';
+import { AgGridReact} from 'ag-grid-react';
+import { useState } from 'react';
+import "ag-grid-community/styles/ag-grid.css";
+
 export const loader = async () => {
 	const prisma = new PrismaClient();
 	const voteTypes = await prisma.voteType.findMany(); 
@@ -10,30 +15,31 @@ export const loader = async () => {
 
 export default function VoteTypeIndex() {
 	const { voteTypes } = useLoaderData<typeof loader>();
-	const list = voteTypes.map(voteType => <tr key={voteType.id}>
-		<td>{voteType.name}</td>
-		<td>{voteType.slug}</td>
-		<td>{voteType.reviewed}</td>
-	</tr>)
+
+	 // Row Data: The data to be displayed.
+	 const [rowData] = useState(voteTypes);
+	
+	// Column Definitions: Defines the columns to be displayed.
+	const [colDefs] = useState<ColDef[]>([
+		{ field: "name" },
+		{ field: "slug" },
+		{ field: "reviewed" }
+	]);
 
 	return (
-		<div className="container">
+		<div className="container mx-auto">
 			<div className="prose">
-				<h1>Vote Types</h1>
-				<table className="table table-sm table-zebra">
-					<thead>
-						<tr>
-							<th>Name</th>
-							<th>Slug</th>
-							<th>Reviewed</th>
-						</tr>
-					</thead>
-					<tbody>
-						{list}
-					</tbody>
-				</table>
-
+				<h1 className='mb-4'>Vote Types</h1>
 			</div>
+				<div
+  className="ag-theme-quartz" // applying the Data Grid theme
+  style={{ height: 500, width: "100%" }} // the Data Grid will fill the size of the parent container
+ >
+   <AgGridReact
+       rowData={rowData}
+       columnDefs={colDefs}
+   />
+ </div>
 		</div>
 		
 	)
