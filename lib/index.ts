55a93559632e1c4b630a415cb3d7d468f62652be
Sean Thompson <sean.thompson@susco.net>
2024-12-16 13:26:35 -0600

    Interim commit. Still getting prismaclient in browser bug.

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
+export { SqlBuilder } from './prisma/sql.server';
 export * from './importer/importer';
 export * from './prisma/db';
 export * from './models';
-export * from './prisma/sql.server';
