d244c020711290a0b968feaf15da6f3b8169276d
Sean Thompson <sean.thompson@susco.net>
2025-04-19 22:10:43 -0500

    [AnimatedBallots] Allegedly fixed runaway rendering.

--- packages/my-fat-senator/app/shared/ballots-list.tsx
+++ packages/my-fat-senator/app/shared/ballots-list.tsx
@@ -8,29 +8,6 @@ interface BallotsListProps {
 	ballots: BallotViewModel[]
 }
 
-export const Canvaser = class {
-
-	static renderToken = (ctx: CanvasRenderingContext2D, 
-		ballot: BallotViewModel,
-		image: HTMLImageElement
-	) => {
-			const dx = ballot.x - ballot.radius;
-			const dy = ballot.y - ballot.radius;
-			ctx.save();
-			ctx.beginPath();
-			ctx.arc(ballot.x, ballot.y, ballot.radius, 0, Math.PI * 2, true);
-			ctx.closePath();
-			ctx.clip();
-			ctx.drawImage(image, dx, dy, ballot.radius * 2, ballot.radius * 2);
-
-			ctx.restore();
-	}
-}
-
-export const LogBallot = (ballot: BallotViewModel) => {
-	console.log("X,Y: ", "(", ballot.x, ",", ballot.y, "), ", ballot.xVelocity, " - ", ballot.yVelocity);
-}
-
 export const BallotsList: React.FC<BallotsListProps> = (props) => {
 	const canvasRef = useRef(null);
 	const [maxWidth] = useState(600);
@@ -38,9 +15,9 @@ export const BallotsList: React.FC<BallotsListProps> = (props) => {
 	const [margin] = useState(30);
 	const [tokensPerLine] = useState(5);
 	const [baseRadius] = useState((maxWidth / tokensPerLine / 10))
-	const [start, setStart] = useState(0);
+	const start = useRef(0);
 
-	const [ballots, setBallots] = useState(props.ballots.map((ballot, index) => {
+	const ballots = useRef(props.ballots.map((ballot, index) => {
 		ballot.xVelocity = Math.floor(Math.random() * 5);
 		ballot.yVelocity = Math.floor(Math.random() * 5);
 		ballot.radius = baseRadius;
@@ -54,63 +31,72 @@ export const BallotsList: React.FC<BallotsListProps> = (props) => {
 
 		ballot.x = cumulativeXMargin;
 		ballot.y = cumulativeYMargin;
-	
+
 		return ballot;
 	}
-))
-  const [isLoaded, setIsLoaded] = useState(false);
-	const imageRef = useRef<HTMLImageElement | null>(null);
+	))
 
-	useEffect(() => {
-    const image = new Image();
-    image.src = "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp";
-    image.onload = () => {
-      setIsLoaded(true);
-      imageRef.current = image;
-    };
-  }, []);
+	const image = useRef<HTMLImageElement | null>(null);
 
+	const renderToken = useCallback((ctx: CanvasRenderingContext2D, 
+		ballot: BallotViewModel,
+		image: HTMLImageElement
+	) => {
+			console.log("useCallback token: ", ballot.legislator.bioguideid)
+			const dx = ballot.x - ballot.radius;
+			const dy = ballot.y - ballot.radius;
+			ctx.save();
+			ctx.beginPath();
+			ctx.arc(ballot.x, ballot.y, ballot.radius, 0, Math.PI * 2, true);
+			ctx.closePath();
+			ctx.clip();
+			ctx.drawImage(image, dx, dy, ballot.radius * 2, ballot.radius * 2);
+
+			ctx.restore();
+	}, [])
 
 	const render = useCallback((ctx: CanvasRenderingContext2D, ts: number) => {
 		const now = ts;
-		const elapsed = now - start;
+		const elapsed = now - start.current;
 		
 		const fps = 30;
 		const fpsInterval = 1000 / fps;
 
 		if (elapsed > fpsInterval) {
-			setStart(now - (elapsed % fpsInterval)); // https://jsfiddle.net/chicagogrooves/nRpVD/2/; resets start to interval marker
+			console.log("useCallback: ", start, ", ", now, ", ", elapsed, ", ", fpsInterval);
+			start.current = (now - (elapsed % fpsInterval));
 			ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-			setBallots(ballots.map((b) => {
-				const ballot = {...b};
-				const rightEdge = (ballot.x + ballot.radius);
-				const leftEdge = (ballot.x - ballot.radius);
-				if (rightEdge >= maxWidth || leftEdge <= 0) {
-					ballot.xVelocity = -1 * ballot.xVelocity;
-				}
-				const bottomEdge = (ballot.y + ballot.radius);
-				const topEdge = (ballot.y - ballot.radius);
-				if (bottomEdge > maxHeight || topEdge < 0) {
-					ballot.yVelocity = -1 * ballot.yVelocity;
-				}
-	
-				ballot.x = ballot.x + ballot.xVelocity;
-				ballot.y = ballot.y + ballot.yVelocity;
-				return ballot;
-			}))
-			ballots.forEach((ballot) => {
-				Canvaser.renderToken(ctx, ballot, imageRef.current!)
+			
+			ballots.current.forEach((ballot) => {
+					const rightEdge = (ballot.x + ballot.radius);
+					const leftEdge = (ballot.x - ballot.radius);
+					if (rightEdge >= maxWidth || leftEdge <= 0) {
+						ballot.xVelocity = -1 * ballot.xVelocity;
+					}
+					const bottomEdge = (ballot.y + ballot.radius);
+					const topEdge = (ballot.y - ballot.radius);
+					if (bottomEdge > maxHeight || topEdge < 0) {
+						ballot.yVelocity = -1 * ballot.yVelocity;
+					}
+
+					ballot.x = ballot.x + ballot.xVelocity;
+					ballot.y = ballot.y + ballot.yVelocity;
+					renderToken(ctx, ballot, image.current!)
 			})
 		}
 
 		return window.requestAnimationFrame((timestamp) => {
 			return render(ctx, timestamp);
 	});
-	}, [ballots, start, maxHeight, maxWidth]);
+	}, [ballots, start, maxHeight, maxWidth, renderToken, image]);
 
   useEffect(() => {
-		if (isLoaded) {
+		console.log("useEffect render")
+
+			image.current = new Image();
+			image.current.src = "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp";
+
 			const canvas = canvasRef.current as HTMLCanvasElement | null;
 			if (canvas == null) {
 				return;
@@ -128,31 +114,10 @@ export const BallotsList: React.FC<BallotsListProps> = (props) => {
 			return () => {
 				window.cancelAnimationFrame(animationFrameId); // TODO How confident are we that this animationFrameId is always the most recent one from inside the loop?
 			}
-		}
 
-  }, [canvasRef, render, isLoaded])
+  }, [image, render])
 
 	return (<div>
-			<div className="debug-panel">
-			<table className="table-auto table-zebra prose w-full max-w-full">
-				<thead>
-					<tr>
-						<th>Legislator</th>
-						<th>Position (X, Y)</th>
-						<th>Velocity (X, Y)</th>
-					</tr>
-				</thead>
-				<tbody>
-				{ballots.map((ballot) => {
-					return <tr key={ballot.legislator.id}>
-						<td>{ballot.legislator.id}</td>
-						<td>{ballot.x}, {ballot.y}</td>
-						<td>{ballot.xVelocity}, {ballot.yVelocity}</td>
-					</tr>
-				})}
-				</tbody>
-			</table>
-			</div>
 			<canvas className='border-solid border' ref={canvasRef} width={maxWidth} height={maxHeight}/>
 		</div>)
 }
