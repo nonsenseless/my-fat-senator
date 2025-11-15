c670f878430738757dbd732acaf58222e8b434da
Sean Thompson <sean.thompson@susco.net>
2025-11-15 17:15:34 -0600

    [Portraits] Address import order.

--- packages/importer/main.ts
+++ packages/importer/main.ts
@@ -3,9 +3,8 @@ import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 import minimist from 'minimist';
 
-import { importVotes } from './services/import-votes';
 import { importLegislators } from './services/import-legislators';
-
+import { importVotes } from './services/import-votes';
 
 const args = minimist(process.argv.slice(2));
 console.log("Starting importer");
