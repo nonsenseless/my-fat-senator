7115ca2dc8bea86183fbd31231c99bdef9933f2b
Sean Thompson <sean.thompson@susco.net>
2025-11-29 13:55:35 -0600

    [Portraits] Interim commit. Tweaking importer main.ts.

--- packages/importer/main.ts
+++ packages/importer/main.ts
@@ -1,10 +1,13 @@
 import 'dotenv/config';
-
+import { CommandLineService } from '@my-fat-senator/lib';
+import { LegislatorService } from "@my-fat-senator/lib/models/legislator";
+import { PartyService } from "@my-fat-senator/lib/models/party";
+import { StateService } from "@my-fat-senator/lib/models/state";
 import { PrismaClient } from '@prisma/client';
 import minimist from 'minimist';
 
-import { importLegislators } from './services/import-legislators';
 import { importVotes } from './services/import-votes';
+import { LegislatorImportService } from './services/legislator-import.service';
 
 const args = minimist(process.argv.slice(2));
 console.log("Starting importer");
@@ -23,9 +26,17 @@ switch (args["command"]) {
 	case "votes":
 		importVotes(prisma, config["TargetDirectory"]);
 		break;
-	case "legislators":
-		importLegislators(prisma);
+	case "legislators": {
+		const stateService = new StateService(prisma);
+		const partyService = new PartyService(prisma);
+		const legislatorService = new LegislatorService(prisma, partyService, stateService);
+		const cli = new CommandLineService();
+
+		const importService = new LegislatorImportService(legislatorService, cli);
+		await importService.importLegislatorBioguides();
+
 		break;
+	}
 	default:
 		console.error("Unrecognized command.");
 		break;
